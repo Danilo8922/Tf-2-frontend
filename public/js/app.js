80eb02dbd41d7e8241465fd22100a32cee41b7ad
Danilo8922 <danlenardial@gmail.com>
2025-08-26 20:50:19 -0300

    tf realizado

--- public/js/app.js
+++ public/js/app.js
@@ -3,9 +3,19 @@ const liElementCollection = ulElement.querySelectorAll("li");
 
 liElementCollection.forEach((liElement) => {
     liElement.addEventListener("click", (event) => {
-        const liElement = event.currentTarget;
+        const itens = document.querySelectorAll('li.item');
 
+        itens.forEach(item => {
+            item.addEventListener('click', (event) => {
+                // Sempre pega o <li> clicado (mesmo se clicou em span/strong)
+                const liClicado = event.currentTarget;
 
-        /** CODAR AQUI */
+                // Remove a classe .selected de todos
+                itens.forEach(i => i.classList.remove('selected'));
+
+                // Adiciona no <li> clicado
+                liClicado.classList.add('selected');
+            });
+        });
     });
 });
